66fd621a04d55ee01dc5f2be682bd03250cae442
Megan Farrington <mlfwarr@gmail.com>
2023-09-05 16:14:49 +0100

    first commit - no styling

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// Import components
+import Navbar from "./components/Navbar";
+import NavItem from "./components/Navbar/NavItem";
+import Logo from "./components/Navbar/Logo";
+import About from "./components/About";
+import HeroDetails from "./components/HeroDetails";
+import Home from "./components/Home";
+
+// Import dependencies
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar logo={<Logo text="*logo*" />}>
+        <NavItem to="/" text="Home" />
+        <NavItem to="/about" text="About" />
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<HeroDetails />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
